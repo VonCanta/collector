245b3bf63dab7f8f00d960c9ff79a0ba4a3641cd
Tucker VonCanon <tucker.voncanon@gmail.com>
2017-04-24 10:42:31 -0400

    initial commit

---
+++ index.java
@@ -0,0 +1,24 @@
+public class Collector
+{
+	public static void main (String [] args)
+	{
+		
+		//declartaion of String array 
+		String[] outputText = new String[5];
+		outputText[0] = "This"; 
+		outputText[1] = "is";
+		outputText[2] = "how";
+		outputText[3] = "to";
+		outputText[4] = "code with collectors"; 
+
+		
+		//demonstrate knowledge of collections 
+		for(int i = 0; i < outputText.length; i++)
+		{
+			System.out.print(outputText[i] + " ");
+		} 
+		System.out.println();
+
+	}
+
+}
